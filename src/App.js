939b022f14ad3017cfa128ba19bb5af86a524023
icchigoo <icchigoo12@gmail.com>
2023-11-02 11:14:25 +0545

    Using  CSS

--- src/App.js
+++ src/App.js
@@ -15,23 +15,19 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div className="container">
-          <div className="row">
-            <div className="col-md-6 offset-md-3">
-              <h2>Login</h2>
-              <form onSubmit={handleSubmit}>
-                <div className="form-group">
-                  <label htmlFor="username">Username</label>
-                  <input type="text" className="form-control" name="username" id="username" required />
-                </div>
-                <div className="form-group">
-                  <label htmlFor="password">Password</label>
-                  <input type="password" className="form-control" name="password" id="password" required />
-                </div>
-                <button type="submit" className="btn btn-primary">Login</button>
-              </form>
+        <div className="login-container">
+          <h2>Login</h2>
+          <form onSubmit={handleSubmit}>
+            <div className="form-group">
+              <label htmlFor="username">Username</label>
+              <input type="text" name="username" id="username" required />
             </div>
-          </div>
+            <div className="form-group">
+              <label htmlFor="password">Password</label>
+              <input type="password" name="password" id="password" required />
+            </div>
+            <button type="submit">Login</button>
+          </form>
         </div>
       </header>
     </div>
