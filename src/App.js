0a30de87c7a94614cbfc2484ffd58c80e812b349
icchigoo <icchigoo12@gmail.com>
2023-11-02 11:10:52 +0545

    Css added

--- src/App.js
+++ src/App.js
@@ -9,7 +9,18 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>Hello, World!</p>
-        <button onClick={displayHelloWorld}>Show Hello, World</button>
+        <button
+          onClick={displayHelloWorld}
+          style={{
+            backgroundColor: 'blue', // Change button background color
+            color: 'white', // Change button text color
+            padding: '10px', // Adjust button padding
+            borderRadius: '5px', // Add rounded corners
+            cursor: 'pointer', // Change cursor on hover
+          }}
+        >
+          Show Hello, World
+        </button>
       </header>
     </div>
   );
