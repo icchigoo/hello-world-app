a661a00a5c3c4a62208bd38c972f853166e0bbed
icchigoo <icchigoo12@gmail.com>
2023-11-02 11:06:05 +0545

    First react app

---
+++ src/SimplePage.js
@@ -0,0 +1,13 @@
+// src/SimplePage.js
+
+import React from 'react';
+
+function SimplePage() {
+  return (
+    <div>
+      <h1>This is a Simple Page</h1>
+    </div>
+  );
+}
+
+export default SimplePage;
