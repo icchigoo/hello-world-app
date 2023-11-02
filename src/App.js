41f6a348dca5d94a5b6f870455c2142ae6add8dc
icchigoo <icchigoo12@gmail.com>
2023-11-02 11:13:05 +0545

    Using bootstrap for login

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
 import './App.css';
 
 function App() {
-  const displayHelloWorld = () => {
-    alert('Hello, World!');
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const username = formData.get('username');
+    const password = formData.get('password');
+    
+    // Replace this with your login logic
+    console.log('Username:', username);
+    console.log('Password:', password);
   };
 
   return (
     <div className="App">
       <header className="App-header">
-        <p>Hello, World!</p>
-        <button
-          onClick={displayHelloWorld}
-          style={{
-            backgroundColor: 'blue', // Change button background color
-            color: 'white', // Change button text color
-            padding: '10px', // Adjust button padding
-            borderRadius: '5px', // Add rounded corners
-            cursor: 'pointer', // Change cursor on hover
-          }}
-        >
-          Show Hello, World
-        </button>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6 offset-md-3">
+              <h2>Login</h2>
+              <form onSubmit={handleSubmit}>
+                <div className="form-group">
+                  <label htmlFor="username">Username</label>
+                  <input type="text" className="form-control" name="username" id="username" required />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="password">Password</label>
+                  <input type="password" className="form-control" name="password" id="password" required />
+                </div>
+                <button type="submit" className="btn btn-primary">Login</button>
+              </form>
+            </div>
+          </div>
+        </div>
       </header>
     </div>
   );
