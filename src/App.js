87ee7f1d17d66cbfdd983be8029be31a9d93a7a8
icchigoo <icchigoo12@gmail.com>
2023-11-07 23:06:38 +0545

    hello world

--- src/App.js
+++ src/App.js
@@ -1,63 +1,21 @@
-import React, { useState } from 'react';
-import './App.css';
+import React from 'react';
 
 function App() {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [loginSuccess, setLoginSuccess] = useState(false);
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setLoginSuccess(true);
+  // Create a function to show an alert
+  const showAlert = () => {
+    alert("Hello, World!");
   };
 
-  const handleUsernameChange = (e) => {
-    setUsername(e.target.value);
-  };
-
-  const handlePasswordChange = (e) => {
-    setPassword(e.target.value);
+  // Create a style object to add color
+  const helloStyle = {
+    color: 'blue',
+    fontSize: '24px',
   };
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <div className="login-container">
-          <h2>Login</h2>
-          <form onSubmit={handleSubmit}>
-            <div className="form-group">
-              <label htmlFor="username">Username</label>
-              <input
-                type="text"
-                name="username"
-                id="username"
-                required
-                value={username}
-                onChange={handleUsernameChange}
-              />
-            </div>
-            <div className="form-group">
-              <label htmlFor="password">Password</label>
-              <input
-                type="password"
-                name="password"
-                id="password"
-                required
-                value={password}
-                onChange={handlePasswordChange}
-              />
-            </div>
-            <button type="submit">Login</button>
-          </form>
-          {loginSuccess && (
-            <div className="success-message">
-              <p>Login Success!</p>
-              <p>Username: {username}</p>
-              <p>Password: {password}</p>
-            </div>
-          )}
-        </div>
-      </header>
+    <div>
+      <h1 style={helloStyle}>Hello World</h1>
+      <button onClick={showAlert}>Show Alert</button>
     </div>
   );
 }
