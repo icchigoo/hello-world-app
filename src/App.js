e77e0f6cbef0ca9b730d0ae4fbc7a36e1b721084
icchigoo <icchigoo12@gmail.com>
2023-11-07 23:16:52 +0545

    login page

--- src/App.js
+++ src/App.js
@@ -1,39 +1,53 @@
 import React, { useState } from 'react';
+import './App.css';
 
 function App() {
-  const [text, setText] = useState(''); // Initialize the state for the text field
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
 
-  const showAlert = () => {
-    alert("Hello, World!");
+  const handleLogin = () => {
+    // Perform authentication logic (e.g., compare with hardcoded values)
+    if (username === 'user' && password === 'password') {
+      setIsAuthenticated(true);
+    } else {
+      setIsAuthenticated(false);
+      alert('Invalid credentials. Please try again.');
+    }
   };
 
-  const helloStyle = {
-    color: 'blue',
-    fontSize: '24px',
-    backgroundColor: 'lightgray',
-    padding: '20px',
-    borderRadius: '8px',
+  const handleUsernameChange = (e) => {
+    setUsername(e.target.value);
   };
 
-  const handleTextChange = (e) => {
-    // Update the 'text' state when the text field value changes
-    setText(e.target.value);
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
   };
 
   return (
     <div>
-      <h1 style={helloStyle}>Hello World</h1>
-      <button onClick={showAlert}>Show Alert</button>
-      
-      {/* Add a text field */}
-      <input
-        type="text"
-        placeholder="Type something..."
-        value={text}
-        onChange={handleTextChange}
-      />
-      
-      <p>You typed: {text}</p>
+      <h1>Login Page</h1>
+      {isAuthenticated ? (
+        <p>You are logged in as {username}.</p>
+      ) : (
+        <div>
+          <input
+            type="text"
+            placeholder="Username"
+            value={username}
+            onChange={handleUsernameChange}
+          />
+          <br />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={handlePasswordChange}
+          />
+          <br />
+          <button onClick={handleLogin}>Login</button>
+        </div>
+      )}
     </div>
   );
 }
