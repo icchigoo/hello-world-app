208254a9c95a7c079e47b6f155ad700417bacedc
icchigoo <icchigoo12@gmail.com>
2023-11-08 23:00:30 +0545

    React route and table page

--- src/App.js
+++ src/App.js
@@ -1,56 +1,83 @@
-import React, { useState } from 'react'; 
-import './App.css'; 
- 
-function App() { 
-  const [username, setUsername] = useState(''); 
-  const [password, setPassword] = useState(''); 
-  const [isAuthenticated, setIsAuthenticated] = useState(false); 
- 
-  const handleLogin = () => { 
-    // Perform authentication logic (e.g., compare with hardcoded values) 
-    if (username === 'user' && password === 'password') { 
-      setIsAuthenticated(true); 
-    } else { 
-      setIsAuthenticated(false); 
-      alert('Invalid credentials. Please try again.'); 
-    } 
-  }; 
- 
-  const handleUsernameChange = (e) => { 
-    setUsername(e.target.value); 
-  }; 
- 
-  const handlePasswordChange = (e) => { 
-    setPassword(e.target.value); 
-  }; 
- 
-  return ( 
-    <div> 
-      <h1>Login Page</h1> 
-      {isAuthenticated ? ( 
-        <p>You are logged in as {username}.</p> 
-      ) : ( 
-        <div> 
-          <input 
-            type="text" 
-            placeholder="Username" 
-            value={username} 
-            onChange={handleUsernameChange} 
-          /> 
-          <br /> 
-          <input 
-            type="password" 
-            placeholder="Password" 
-            value={password} 
-            onChange={handlePasswordChange} 
-          /> 
-          <br /> 
-          <button onClick={handleLogin}>Login</button> 
-        </div> 
-      )} 
-    </div> 
-  ); 
-} 
- 
-export default App; 
- 
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';
+import TablePage from './TablePage';
+
+function App() {
+  const navigate = useNavigate();
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const handleLogin = () => {
+    // Perform authentication logic (e.g., compare with hardcoded values)
+    if (username === 'user' && password === 'password') {
+      setIsAuthenticated(true);
+      navigate('/table'); // Navigate to the table page on successful login
+    } else {
+      setIsAuthenticated(false);
+      alert('Invalid credentials. Please try again.');
+    }
+  };
+
+  const handleUsernameChange = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  return (
+    <div>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/table">Table Page</Link>
+        </li>
+      </ul>
+
+      <hr />
+
+      <Routes>
+        <Route path="/" element={<Home username={username} isAuthenticated={isAuthenticated} handleUsernameChange={handleUsernameChange} handlePasswordChange={handlePasswordChange} handleLogin={handleLogin} />} />
+        <Route path="/table" element={<TablePage />} />
+      </Routes>
+    </div>
+  );
+}
+
+function Home({ username, isAuthenticated, handleUsernameChange, handlePasswordChange, handleLogin }) {
+  // Remove the 'password' state from here
+  return (
+    <div>
+      <h1>Login Page</h1>
+      {isAuthenticated ? (
+        <p>You are logged in as {username}.</p>
+      ) : (
+        <div>
+          <input
+            type="text"
+            placeholder="Username"
+            value={username}
+            onChange={handleUsernameChange}
+          />
+          <br />
+          <input
+            type="password"
+            placeholder="Password"
+            // Use the handlePasswordChange function to update the password
+            onChange={handlePasswordChange}
+          />
+          <br />
+          <button onClick={handleLogin}>Login</button>
+        </div>
+      )}
+    </div>
+  );
+}
+
+
+export default App;
---
+++ src/TablePage.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+function TablePage() {
+  // Sample table data
+  const tableData = [
+    { id: 1, name: 'Item 1', description: 'Description 1' },
+    { id: 2, name: 'Item 2', description: 'Description 2' },
+    { id: 3, name: 'Item 3', description: 'Description 3' },
+  ];
+
+  return (
+    <div>
+      <h1>Table Page</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Description</th>
+          </tr>
+        </thead>
+        <tbody>
+          {tableData.map((item) => (
+            <tr key={item.id}>
+              <td>{item.id}</td>
+              <td>{item.name}</td>
+              <td>{item.description}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default TablePage;
