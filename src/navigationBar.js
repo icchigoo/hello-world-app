710d4676d5df43b7fa92f0aa6fbce2106cdf7a29
icchigoo <icchigoo12@gmail.com>
2023-11-20 11:33:12 +0545

    Navigation Bar

--- src/navigationBar.js
+++ src/navigationBar.js
@@ -1,24 +1,34 @@
-// NavigationBar.js
+// navigationBar.js
 
 import React from "react";
 import { Link } from "react-router-dom";
+import "./navigationBar.css"; // Import the associated CSS file
 
 function NavigationBar() {
   return (
-    <div>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
+    <div className="navbar-container">
+      {/* Navigation bar list with links */}
+      <ul className="navbar-list">
+        {/* Individual navigation bar items */}
+        <li className="navbar-item">
+          <Link to="/" className="navbar-link">
+            Home
+          </Link>
         </li>
-        <li>
-          <Link to="/table">Table Page</Link>
+        <li className="navbar-item">
+          <Link to="/table" className="navbar-link">
+            Table Page
+          </Link>
         </li>
-        <li>
-          <Link to="/form-elements">Form Elements Page</Link>{" "}
+        <li className="navbar-item">
+          <Link to="/form-elements" className="navbar-link">
+            Form Elements Page
+          </Link>{" "}
         </li>
       </ul>
 
-      <hr />
+      {/* Divider line to separate sections of the navigation bar */}
+      <hr className="navbar-divider" />
     </div>
   );
 }
